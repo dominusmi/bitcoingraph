ce76f3ecb0960db7550464d6140b5bcf12b30d61
Ed <barp.edoardo@gmail.com>
2023-06-28 15:16:41 +0200

    Refactor code into separate script & src

--- bitcoingraph/entities.py
+++ bitcoingraph/entities.py
@@ -1,10 +1,15 @@
 import bisect
 import csv
 import os
+import queue
+import threading
+from time import sleep
+
+import neo4j
+import tqdm
 
 
 class Address:
-
     counter = 0
 
     def __init__(self, address, assign_number=False):
@@ -149,3 +154,140 @@ def calculate_input_addresses(input_path):
 
             if match_address is not None:
                 input_address_writer.writerow([txid, match_address])
+
+
+def fetch_outputs_thread(session: neo4j.Session, batch_size: int, start_height: int, max_height: int,
+                         result_queue: queue.Queue, stop_queue: queue.Queue):
+    query = """
+        MATCH (b:Block)
+        WHERE b.height >= $lower AND b.height < $higher
+        WITH b
+        LIMIT $higher-$lower
+        MATCH (b)-[:CONTAINS]->(t)<-[:INPUT]-(o)-[:USES]->(a)
+        RETURN t.txid as txid, collect(a.address) as addresses
+        """
+
+    idx = start_height
+    try:
+        while stop_queue.empty() and idx < max_height:
+            result = session.run(query, stream=True, lower=idx, higher=idx + batch_size)
+            data = result.data()
+            result_queue.put(data)
+            idx += batch_size
+
+    except Exception as e:
+        print(f"Thread failed: {e}")
+    finally:
+        result_queue.put(None)
+        session.close()
+
+
+def add_entities(batch_size: int, start_height: int, max_height: int, driver: neo4j.Driver):
+    entity_idx_counter = 0
+    entity_idx_to_addresses = {}  # in theory, could be a list, but slightly more complex logic for little gain
+    address_to_entity_idx = {}
+
+    session = driver.session()
+    result_queue = queue.Queue()
+    stop_queue = queue.Queue()
+
+    if max_height is None:
+        max_height = session.run("MATCH (b:Block) RETURN max(b.height) as maxHeight").data()[0]["maxHeight"]
+
+    # This is the thread that continuously queries for the next batch_size blocks, and returns the outputs + addresses
+    thread = threading.Thread(target=fetch_outputs_thread,
+                              args=(session, batch_size, start_height, max_height, result_queue, stop_queue,))
+    thread.start()
+
+    counter_joined_entities = 0
+    counter_entities = 0
+
+    try:
+        loop_counter = 0
+        progress_bar = tqdm.tqdm(total=max_height)
+        while True:
+            try:
+                result_list = result_queue.get_nowait()
+                if result_list is None:
+                    break
+                for result in result_list:
+                    txid = result["txid"]
+                    addresses = result["addresses"]
+
+                    found_entities_idx = []
+                    for addr in addresses:
+                        entity_idx = address_to_entity_idx.get(addr, None)
+                        if entity_idx is not None:
+                            found_entities_idx.append(entity_idx)
+
+                    if found_entities_idx:
+                        # Here we need to join all the addresses from the different entities together
+                        min_entity_idx = min(found_entities_idx)
+                        entity_address_set = entity_idx_to_addresses[min_entity_idx]
+                        for entity_idx in found_entities_idx:
+                            if entity_idx == min_entity_idx:
+                                continue
+                            entity_address_set.update(entity_idx_to_addresses[entity_idx])
+                            entity_idx_to_addresses.pop(entity_idx)
+
+                        for addr in addresses:
+                            address_to_entity_idx[addr] = min_entity_idx
+
+                        counter_entities -= (len(found_entities_idx) - 1)
+                        counter_joined_entities += len(found_entities_idx)
+
+                    else:
+                        if len(addresses) <= 1:
+                            continue
+                        # create a new entity
+                        entity_idx = entity_idx_counter
+                        for addr in addresses:
+                            address_to_entity_idx[addr] = entity_idx
+                        entity_idx_to_addresses[entity_idx] = set(addresses)
+                        entity_idx_counter += 1
+                        counter_entities += 1
+
+                progress_bar.update(batch_size)
+
+            except queue.Empty:
+                print("-", end="")
+                sleep(1)
+
+            loop_counter += 1
+            # if loop_counter % 50 == 0:
+            progress_bar.set_postfix({'Total entities': counter_entities, 'Counter joined': counter_joined_entities})
+
+    except KeyboardInterrupt:
+        stop_queue.put("Time to stop")
+        for i in range(10):
+            if thread.is_alive():
+                sleep(2)
+
+    with driver.session() as session:
+        for entity_idx, addresses in entity_idx_to_addresses.items():
+            if len(addresses) <= 1:
+                continue
+            session.run("""
+            MATCH (a:Address)
+            WHERE a.address in $addresses
+            WITH a
+            OPTIONAL MATCH (e:Entity)--(a)
+            CALL {
+                WITH a,e
+                WITH a,e as entity WHERE entity IS NOT NULL
+                MERGE (a)-[:BELONGS_TO]->(entity)
+                RETURN entity
+
+                UNION 
+
+                WITH a,e
+                WITH a,e WHERE e IS NULL
+                WITH collect(a) as addresses
+                MERGE (entity:Entity {representative: $representative})
+                WITH entity, addresses
+                UNWIND addresses as a
+                MERGE (entity)<-[:BELONGS_TO]-(a)
+                RETURN entity
+            }
+            RETURN entity
+            """, addresses=list(addresses), representative=min(addresses))
