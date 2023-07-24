f42d67df1ebf5cd316cb469a75fd1a18db796801
Ed <barp.edoardo@gmail.com>
2023-07-24 10:07:37 +0200

    Fix init

--- bitcoingraph/__init__.py
+++ bitcoingraph/__init__.py
@@ -6,8 +6,6 @@ A Python library for exploring the Bitcoin transaction graph.
 """
 import platform
 
-from bitcoingraph.bitcoingraph import BitcoinGraph
-
 __author__ = 'Bernhard Haslhofer, Roman Karl'
 __license__ = "MIT"
 __version__ = '0.3.2dev'
