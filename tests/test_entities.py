e8596f3336c8adf4c65c0747fb56a000b010141f
Ed <barp.edoardo@gmail.com>
2023-08-23 11:15:49 +0200

    Add test for entities queries

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -1,7 +1,7 @@
 import neo4j
 import pytest
 
-from bitcoingraph.entities import EntityGrouping
+from bitcoingraph.entities import EntityGrouping, get_addresses_grouped_by_transaction
 
 
 @pytest.fixture
@@ -15,6 +15,31 @@ def session():
         yield session
 
     driver.close()
+
+def test_get_addresses_grouped_by_transaction(session):
+    session.run("""
+    MERGE (b1:Block {height: 1})-[:CONTAINS]->(t1:Transaction {txid: 1})
+    MERGE (t1)<-[:INPUT]-(o11:Output)
+    MERGE (t1)<-[:INPUT]-(o12:Output)
+    MERGE (o11)-[:USES]->(a11:Address {address: "11"})
+    MERGE (o12)-[:USES]->(a12:Address {address: "12"})
+    MERGE (a11)-[:GENERATES]->(a11g:Address {address: "11_1g"})
+    MERGE (a11)-[:GENERATES]->(a12g:Address {address: "11_2g"})
+    MERGE (a12f:Address {address: "12father"})-[:GENERATES]->(a12)
+    MERGE (b1)-[:CONTAINS]->(t2:Transaction {txid: 2})
+    MERGE (t2)<-[:INPUT]-(o21:Output)
+    MERGE (t2)<-[:INPUT]-(o22:Output)
+    MERGE (o21)-[:USES]->(a21:Address {address: "21"})
+    MERGE (o22)-[:USES]->(a22:Address {address: "22"})
+    """)
+
+    rows = get_addresses_grouped_by_transaction(session, 1, 1)
+    assert len(rows) == 2
+    assert set(rows[0]["addresses"]) == {'11', '12'}
+    assert set(rows[0]["generated"]) == {'11_1g', '11_2g', '12father'}
+
+    assert set(rows[1]["addresses"]) == {'21', '22'}
+    assert set(rows[1]["generated"]) == set([])
 
 
 class TestSaveEntities():
@@ -237,3 +262,52 @@ class TestSaveEntities():
         assert data[0]["e"]["name"] == "foo"
         assert len(data[0]["addresses"])
         assert set((x["address"] for x in data[0]["addresses"])) == {"1", "2", "3"}
+
+
+class TestMergeEntities:
+    def test_simple_case(self, session):
+        session.run("""
+                // Two entities
+                MERGE (a1:Address {address: "1"})<-[:OWNER_OF]-(e1:Entity {entity_id: 1})
+                MERGE (a2:Address {address: "2"})<-[:OWNER_OF]-(e2:Entity {entity_id: 2})
+                MERGE (a1)-[:GENERATES]->(a2)
+                """)
+
+        session.run("""
+        MATCH (e1:Entity)-[:OWNER_OF]->(a1:Address)-[:GENERATES]->(a2:Address)<-[:OWNER_OF]-(e2:Entity)
+        WITH [e1,e2] as entities
+        CALL apoc.refactor.mergeNodes(entities)
+        YIELD node
+        RETURN node
+        """)
+
+    def test_generator_has_entity(self, session):
+        session.run("""
+        MERGE (a1:Address {address: "1"})<-[:OWNER_OF]-(e1:Entity {entity_id: 1})
+        MERGE (a1)-[:GENERATES]->(a2)
+        """)
+
+    def test_generated_has_entity(self, session):
+        session.run("""
+        MERGE (a1:Address {address: "1"})<-[:OWNER_OF]-(e1:Entity {entity_id: 1})
+        MERGE (a2:Address {address: "2"})<-[:OWNER_OF]-(e2:Entity {entity_id: 2})
+        MERGE (a1)-[:GENERATES]->(a2)
+        """)
+
+    def test_circular_dependence(self, session):
+        merge_query = []
+        for i in range(6):
+            merge_query.append(f"MERGE (a{i+1}:Address {{address: '{i}'}})")
+
+        merge_query = "\n".join(merge_query)
+        session.run(merge_query + """
+        MERGE (a1)<-[:OWNER_OF]-(e1:Entity {entity_id: 1})
+        MERGE (a1)-[:GENERATES]->(a2)
+        MERGE (a2)<-[:OWNER_OF]-(e2:Entity {entity_id: 2})
+        MERGE (e2)-[:OWNER_OF]->(a3)
+        MERGE (a3)-[:GENERATES]->(a4)
+        MERGE (a4)<-[:OWNER_OF]-(e3:Entity {entity_id: 3})
+        MERGE (e3)-[:OWNER_OF]->(a5)
+        MERGE (a5)<-[:GENERATES]-(a6)
+        MERGE (e1)-[:OWNER_OF]->(a6)
+        """)
